9eeb7311959b5ebf74c1b62bc9acd701c0dfe404
TDarkShadow <5389859+TDarkShadow@users.noreply.github.com>
2019-10-27 22:12:38 +0100

    ✨First commit!

---
+++ index.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.on("ready", () => {
+  console.log("I am ready to flag!");
+});
+
+const forbiddenLetter = "e";
+const reaction = "🏴";
+
+client.on("message", message => {
+  if (message.author.bot) return;
+  if (message.content.toLowerCase().includes(forbiddenLetter)) {
+    message.react(reaction);
+  }
+});
+
+client.login("process.env.TOKEN");
